90da7376a99b1b2b265b5f1951e65573ea818f79
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2019-12-26 21:39:43 -0300

    Add Code struct

--- src/assembler.rs
+++ src/assembler.rs
@@ -9,44 +9,70 @@ fn big_to_little_endiann(value: u16) -> (u8, u8) {
     ((value & 0xFF) as u8, ((value & 0xFF00) >> 8) as u8)
 }
 
+struct Code {
+    cart: [u8; 0x10000],
+    pointer: usize,
+}
+impl Code {
+    fn new() -> Self {
+        Self {
+            cart: [0x00u8; 0x10000],
+            pointer: 0,
+        }
+    }
+    /// Place a u8 on self.pointer, then increment by 1
+    fn push_byte(&mut self, byte: u8) {
+        self.cart[self.pointer] = byte;
+        self.pointer += 1;
+    }
+    /// Take a u16, convert it to little endiand then place it on self.pointer, finally increment by 2
+    fn push_long(&mut self, long: u16) {
+        let long = big_to_little_endiann(long);
+        self.push_byte(long.0);
+        self.push_byte(long.1);
+    }
+    /// Add <amount> to self.pointer
+    fn skip(&mut self, amount: usize) {
+        self.pointer += amount;
+    }
+}
+
 pub fn assemble(parsed_code: Vec<LineType>) -> Result<[u8; 0x10000], Error> {
     // cart: holds the code
     // labels: label=address
     // labels_used_on: places where a label was used
     // addr: where to place the code
-    let mut cart: [u8; 0x10000] = [0x00; 0x10000];
+    let mut code = Code::new();
     let mut labels: HashMap<String, usize> = HashMap::default();
     let mut labels_used_on: HashMap<String, Vec<usize>> = HashMap::default();
-    let mut addr = 0x8000;
 
     for line in parsed_code {
         match line {
             LineType::LabelDef(name) => {
-                labels.insert(name.clone(), addr);
+                labels.insert(name.clone(), code.pointer);
             }
             LineType::Opcode(opcode) => {
-                let code = get_code(opcode.name, opcode.arg.0)?;
+                let opcode_number = get_code(opcode.name, opcode.arg.0)?;
                 let size = addressing_modes::get_size(opcode.arg.0);
-                cart[addr] = code;
+                code.push_byte(opcode_number);
                 match &opcode.arg.1 {
-                    Value::Long(long) => {
-                        let long = big_to_little_endiann(*long);
-                        cart[addr + 1] = long.0;
-                        cart[addr + 2] = long.1;
-                    }
-                    Value::Short(short) => cart[addr + 1] = *short,
+                    Value::Long(long) => code.push_long(*long),
+                    Value::Short(short) => code.push_byte(*short),
                     Value::Label(name) => {
                         if let Some(addresses) = labels_used_on.get_mut(name) {
-                            addresses.push(addr);
+                            addresses.push(code.pointer);
                         } else {
-                            labels_used_on.insert(name.clone(), [addr].to_vec());
+                            labels_used_on.insert(name.clone(), [code.pointer].to_vec());
                         }
+                        // Since we don't know what to place here, just skip the argument (-1 for the opcode)
+                        code.skip(size - 1);
                     }
                     Value::None => {}
+                    Value::Array(_) => panic!("Array?"),
                 }
-                addr += size;
-                println!("Assembling {:?} as {:#04X}", opcode, code);
+                println!("Assembling {:?} as {:#04X}", opcode, opcode_number);
             }
+            _ => {}
         };
     }
     // Iterate thru all the defined labels
@@ -59,10 +85,9 @@ pub fn assemble(parsed_code: Vec<LineType>) -> Result<[u8; 0x10000], Error> {
             None => continue,
             Some(v) => v,
         };
-        let addr_little = big_to_little_endiann(labels[label] as u16);
         for address in addresses_where_used {
-            cart[address + 1] = addr_little.0;
-            cart[address + 2] = addr_little.1;
+            code.pointer = address + 1;
+            code.push_long(labels[label] as u16);
         }
     }
     if !labels_used_on.is_empty() {
@@ -70,7 +95,7 @@ pub fn assemble(parsed_code: Vec<LineType>) -> Result<[u8; 0x10000], Error> {
             labels: format!("{:?}", labels_used_on),
         });
     }
-    Ok(cart)
+    Ok(code.cart)
 }
 
 mod tests {
