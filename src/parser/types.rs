efe08ce7ead596dc039ceaf9b2e4889fe441750b
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2020-01-31 00:29:05 -0300

    Added Value::Text and is_... to Value

--- src/parser/types.rs
+++ src/parser/types.rs
@@ -8,7 +8,56 @@ pub enum Value {
     Long(u16),
     Label(String),
     Array(Vec<Value>),
+    Text(Box<[u8]>),
     None,
+}
+
+impl Value {
+    pub fn is_short(&self) -> bool {
+        match self {
+            Value::Short(_) => true,
+            Value::Long(_) => false,
+            Value::Label(_) => false,
+            Value::Array(_) => false,
+            Value::None => false,
+        }
+    }
+    pub fn is_long(&self) -> bool {
+        match self {
+            Value::Short(_) => false,
+            Value::Long(_) => true,
+            Value::Label(_) => false,
+            Value::Array(_) => false,
+            Value::None => false,
+        }
+    }
+    pub fn is_label(&self) -> bool {
+        match self {
+            Value::Short(_) => false,
+            Value::Long(_) => false,
+            Value::Label(_) => true,
+            Value::Array(_) => false,
+            Value::None => false,
+        }
+    }
+    pub fn is_array(&self) -> bool {
+        match self {
+            Value::Short(_) => false,
+            Value::Long(_) => false,
+            Value::Label(_) => false,
+            Value::Array(_) => true,
+            Value::None => false,
+        }
+    }
+    pub fn is_none(&self) -> bool {
+        match self {
+            Value::Short(_) => false,
+            Value::Long(_) => false,
+            Value::Label(_) => false,
+            Value::Array(_) => false,
+            Value::None => true,
+        }
+    }
 }
 pub type ArgumentType = (AddressingMode, Value);
 
